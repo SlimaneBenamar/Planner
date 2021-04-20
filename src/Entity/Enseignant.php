5f783e15e5727ce51cb22ba5aa1bbf523381fed7
Slimane Benamar <slimanebnr@gmail.com>
2021-04-20 12:24:02 +0200

    ajout entity enseignant

---
+++ src/Entity/Enseignant.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EnseignantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EnseignantRepository::class)
+ */
+class Enseignant
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idEnseignant;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nomEnseignant;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenomEnseignant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdEnseignant(): ?int
+    {
+        return $this->idEnseignant;
+    }
+
+    public function setIdEnseignant(int $idEnseignant): self
+    {
+        $this->idEnseignant = $idEnseignant;
+
+        return $this;
+    }
+
+    public function getNomEnseignant(): ?string
+    {
+        return $this->nomEnseignant;
+    }
+
+    public function setNomEnseignant(string $nomEnseignant): self
+    {
+        $this->nomEnseignant = $nomEnseignant;
+
+        return $this;
+    }
+
+    public function getPrenomEnseignant(): ?string
+    {
+        return $this->prenomEnseignant;
+    }
+
+    public function setPrenomEnseignant(string $prenomEnseignant): self
+    {
+        $this->prenomEnseignant = $prenomEnseignant;
+
+        return $this;
+    }
+}
